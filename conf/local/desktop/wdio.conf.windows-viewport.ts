126d01e58d8e73c92b7fa626edaabd9515d27697
feeroz123 <feeroz123@gmail.com>
2025-06-04 19:11:13 +0530

    Updated Chrome browser version to 137

--- conf/local/desktop/wdio.conf.windows-viewport.ts
+++ conf/local/desktop/wdio.conf.windows-viewport.ts
@@ -15,7 +15,7 @@ config.capabilities = [{
 // All 3 parameters are required in this method for the viewport sizing to work
 config.before = async function (capabilities, specs, browser) {
     await browser.setViewport({ width: 320, height: 642 })
-    console.log("Viewport size is set")
+    console.log("Viewport size is set to 320x642")
 }
 
 exports.config = config
